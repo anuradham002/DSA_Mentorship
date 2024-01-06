526b6331d124726056ece9f905c2c81177724f3a
Anuradha <144935088+anuradham002@users.noreply.github.com>
2024-01-06 23:59:30 +0530

    Create q6.cpp

---
+++ Assignment_2/q6.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n,N;
+    cout<<"enter value of n: ";
+    cin >> n;
+    N=n/2+1;
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=1;j<=n-1;j++)
+        {
+            if(j<n-i)
+            {
+            cout<<"* ";
+            }
+            else{
+            cout<<"  ";
+            }
+        }
+        for(int k=n-2;k>=1;k--)
+        {
+            if(k<n-i)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }   
+    for(int i=N-1;i>=1;i--)
+    {
+        for(int j=1;j<=n-1;j++)
+        {
+            if(j<n-i)
+            {
+            cout<<"* ";
+            }
+            else{
+            cout<<"  ";
+            }
+        }
+        for(int k=n-2;k>=1;k--)
+        {
+            if(k<n-i)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }
+}    

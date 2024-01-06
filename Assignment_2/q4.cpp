2949cc27384a1394c381ecd8bef1ffe221dbb344
Anuradha <144935088+anuradham002@users.noreply.github.com>
2024-01-06 22:48:33 +0530

    Create q4.cpp

---
+++ Assignment_2/q4.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter value of n: ";
+    cin >> n;
+    for(int i=n;i>=1;i--)
+    {
+        for(int j=n;j>=1;j--)
+        {
+            if(j<=i)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }   
+}

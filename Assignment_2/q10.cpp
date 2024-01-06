0f7e524e795414aa54978cc57fb964dcd2002ad3
Anuradha <144935088+anuradham002@users.noreply.github.com>
2024-01-07 02:14:22 +0530

    Update q10.cpp

--- Assignment_2/q10.cpp
+++ Assignment_2/q10.cpp
@@ -1,1 +1,45 @@
+#include <iostream>
+using namespace std;
 
+int main(){
+    int n,N;
+    cout<<"enter value of n: ";
+    cin >> n;
+    N=n/2+1;
+    for(int i=1;i<=N;i++)
+    {
+        for(int j=N;j>=1;j--)
+        {
+            if(j==i) 
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        for(int k=2;k<=N;k++)
+        {
+            if(k==i)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }    
+    for(int i=N+1;i<=n;i++)
+    {
+        for(int j=N;j>=1;j--)
+        {
+            if(j==n-i+1)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        for(int k=2;k<=N;k++)
+        {
+            if(k==n-i+1)
+            cout<<"* ";
+            else
+            cout<<"  ";
+        }
+        cout<<endl;
+    }   
+}

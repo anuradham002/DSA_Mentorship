7c0b15a5085f1ab194f04198348e0c7e0d0313a7
Anuradha <144935088+anuradham002@users.noreply.github.com>
2024-01-07 00:43:24 +0530

    Create q11.cpp

---
+++ Assignment_2/q11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cout<<"enter value of n: ";
+    cin >> n;
+    int a=0;
+    for(int i=1;i<=n;i++)
+    {
+        for(int j=1;j<=n;j++)
+        {
+           
+            if(j<=i){
+             a=a+1;
+            cout<<a<<" ";
+            }
+        }
+        cout<<endl;
+    }   
+}
